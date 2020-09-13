7d8ae5ee9b4ec45c5b49726fd0c4ce1affc9381e
DosGo <dosgo@qq.com>
2020-09-13 21:21:48 +0800

    。

--- dnspod/dnspod.go
+++ dnspod/dnspod.go
@@ -7,8 +7,10 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 
@@ -179,17 +181,53 @@ func (dp *DnsPod) DdnsUpdate(iptype int,domain string,subDomain string){
 		ipv4,errv4:=iptool.GetPublicIP(4);
 		if(errv4==nil&&len(ipv4)>0){
 			fmt.Printf("find ipv4 address")
-			err:=dp.Modify(domain,ipv4,subDomain,"A");
-			fmt.Printf("err:%#v\r\n",err)
+			if(dp.CheckIP(4,ipv4,domain,subDomain)==false) {
+				err := dp.Modify(domain, ipv4, subDomain, "A");
+				fmt.Printf("err:%#v\r\n",err)
+			}else{
+				fmt.Printf("No changes\r\n")
+			}
 		}
 	}
 	if(iptype==6||iptype==0){
 		ipv6,errv6:=iptool.GetPublicIP(6);
 		if(errv6==nil&&len(ipv6)>0){
 			fmt.Printf("find ipv6 address")
-			err:=dp.Modify(domain,ipv6,subDomain,"AAAA");
-			fmt.Printf("err:%#v\r\n",err)
+			//Modify  update
+			if(dp.CheckIP(6,ipv6,domain,subDomain)==false) {
+				err := dp.Modify(domain, ipv6, subDomain, "AAAA");
+				fmt.Printf("err:%#v\r\n", err)
+			}else{
+				fmt.Printf("No changes\r\n")
+			}
 		}
 	}
 }
 
+
+func (dp *DnsPod) CheckIP(iptype int,caddr string,domain string,subDomain string)bool{
+	var network="ip";
+	switch iptype {
+		case 4:
+		network="ip4"
+		break;
+		case 6:
+		network="ip6"
+		break;
+	}
+	addr,err:=net.ResolveIPAddr(network,subDomain+"."+domain);
+	if(err!=nil){
+		return false;
+	}
+	if(iptype==6){
+		if(strings.Index(addr.String(),":")!=-1&&caddr==addr.String()){
+			return true;
+		}
+	}
+	if(iptype==4){
+		if(strings.Index(addr.String(),":")==-1&&caddr==addr.String()){
+			return true;
+		}
+	}
+	return false;
+}
