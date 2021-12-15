9e24eb1c5578937c0d3b02199c1969fd51bef810
DosGo <dosgo@qq.com>
2021-12-15 22:05:07 +0800

    use tinygo http

---
+++ ddns/tinyhttp.go
@@ -0,0 +1,69 @@
+package ddns
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"tinygo.org/x/drivers/net/http"
+)
+
+func post(_url string, contentType string, data []byte, headers map[string]string) ([]byte, error) {
+	if contentType == "" {
+		contentType = "application/x-www-form-urlencoded"
+	}
+	req, err := http.NewRequest("POST", _url, bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", contentType)
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Add(k, v)
+		}
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
+}
+
+func put(_url string, contentType string, data []byte, headers map[string]string) ([]byte, error) {
+	req, err := http.NewRequest("PUT", _url, bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", contentType)
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Add(k, v)
+		}
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
+}
+
+func get(url string, headers map[string]string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Add(k, v)
+		}
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/chai2010/winsvc v0.0.0-20200705094454-db7ec320025c
-	tinygo.org/x/drivers v0.17.2-0.20210919110538-3fe3fc64e4de
+	tinygo.org/x/drivers v0.18.0
 )
